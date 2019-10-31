19392a9e4247a1a4383a4337dcce50c83d0dca5a
catena <catena2w@users.noreply.github.com>
2019-10-31 16:39:39 +0300

    check address validity (#13)

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,13 +10,32 @@ import {
   sendWithoutBoxId,
   getBoxesFromFewSks,
   createOutputs,
-  getAssetsFromBoxes,
+  getAssetsFromBoxes, checkAddressValidity,
 } from '../src/index';
 import { sign, verify } from '../src/ergoSchnorr';
 import { testNetServer } from '../src/api';
 
 const testAddress = '3WxxVQqxoVSWEKG5B73eNttBX51ZZ6WXLW7fiVDgCFhzRK8R4gmk';
 const testSK = '8e6993a4999f009c03d9457ffcf8ff3d840ae78332c959c8e806a53fbafbbee1';
+
+test('simple address validity test', () => {
+  const addresses = [
+    ['2Z4YBkDsDvQj8BX7xiySFewjitqp2ge9c99jfes2whbtKitZTxdBYqbrVZUvZvKv6aqn9by4kp3LE1c26LCyosFnVnm6b6U1JYvWpYmL2ZnixJbXLjWAWuBThV1D6dLpqZJYQHYDznJCk49g5TUiS4q8khpag2aNmHwREV7JSsypHdHLgJT7MGaw51aJfNubyzSKxZ4AJXFS27EfXwyCLzW1K6GVqwkJtCoPvrcLqmqwacAWJPkmh78nke9H4oT88XmSbRt2n9aWZjosiZCafZ4osUDxmZcc5QVEeTWn8drSraY3eFKe8Mu9MSCcVU', true],
+    ['88dhgzEuTXaSLUWK1Ro8mB5xfhwP4y8osUycdBV16EBgycjcBebwd2He7QGiXC1qiSM1KZ6bAcpE2iCv', true],
+    ['9fRusAarL1KkrWQVsxSRVYnvWxaAT2A96cKtNn9tvPh5XUyCisr', true],
+    ['9fRusAarL1KkrWQVsxSRVYnvWxaAT2A96cKtNn9tvPh5XUyCiss', false],
+    ['9fRusAarL1KkrWQVsxSRVYnvWxaAT2A96c', false],
+    ['', false],
+    [1234, false],
+    [Infinity, false],
+    [true, false],
+    [null, false],
+  ];
+
+  addresses.forEach((address) => {
+    expect(checkAddressValidity(address[0])).toBe(address[1]);
+  });
+});
 
 test('P2PK wallet in test network test vector', () => {
   expect(
